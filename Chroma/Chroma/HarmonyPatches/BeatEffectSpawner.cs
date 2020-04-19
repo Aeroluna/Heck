108ed5359d6681d6e7a6c73f1fd200e4ee3d2f88
Aeroluna <aeroluna5@gmail.com>
2020-04-19 11:11:25 -0700

    fixed note spawn effect

---
+++ Chroma/Chroma/HarmonyPatches/BeatEffectSpawner.cs
@@ -0,0 +1,28 @@
+﻿using Chroma.Events;
+using Chroma.Settings;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Chroma.HarmonyPatches
+{
+    [HarmonyPatch(typeof(BeatEffectSpawner))]
+    [HarmonyPatch("HandleNoteDidStartJumpEvent")]
+    internal class HandleNoteDidStartJumpEvent
+    {
+        private static void Prefix(NoteController noteController)
+        {
+            if (!ColourManager.TechnicolourBlocks || ChromaConfig.TechnicolourBlocksStyle != ColourManager.TechnicolourStyle.GRADIENT)
+            {
+                if (ChromaNoteColourEvent.SavedNoteColours.TryGetValue(noteController, out Color c))
+                {
+                    ColourManager.SetNoteTypeColourOverride(noteController.noteData.noteType, c);
+                }
+            }
+        }
+
+        private static void Postfix(NoteController noteController)
+        {
+            ColourManager.RemoveNoteTypeColourOverride(noteController.noteData.noteType);
+        }
+    }
+}
