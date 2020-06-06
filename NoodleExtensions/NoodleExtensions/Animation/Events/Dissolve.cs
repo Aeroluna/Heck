a82e21b716dae0c7859912ea7661aaa4edeaef2d
Aeroluna <aeroluna5@gmail.com>
2020-06-05 18:42:09 -0700

    fixy dissolve

--- NoodleExtensions/NoodleExtensions/Animation/Events/Dissolve.cs
+++ NoodleExtensions/NoodleExtensions/Animation/Events/Dissolve.cs
@@ -29,26 +29,13 @@ namespace NoodleExtensions.Animation
                     string easingString = Trees.at(customEventData.data, EASING);
                     Easings.Functions easing = Easings.InterprateString(easingString);
 
-                    List<CutoutAnimateEffect> cutoutAnimateEffects = new List<CutoutAnimateEffect>();
-                    foreach (NoteController noteController in GetActiveNotes(track))
-                    {
-                        BaseNoteVisuals baseNoteVisuals = noteController.gameObject.GetComponent<BaseNoteVisuals>();
-                        cutoutAnimateEffects.Add(_noteCutoutAnimateEffectAccessor(ref baseNoteVisuals));
-                    }
-                    foreach (ObstacleController obstacleController in GetActiveObstacles(track))
-                    {
-                        ObstacleDissolve obstacleDissolve = obstacleController.gameObject.GetComponent<ObstacleDissolve>();
-                        cutoutAnimateEffects.Add(_obstacleCutoutAnimateEffectAccessor(ref obstacleDissolve));
-                    }
-
                     if (_activeCoroutines.TryGetValue(track, out Coroutine coroutine) && coroutine != null) _instance.StopCoroutine(coroutine);
-                    _activeCoroutines[track] = _instance.StartCoroutine(DissolveCoroutine(start, end, duration, customEventData.time, cutoutAnimateEffects, easing, track));
+                    _activeCoroutines[track] = _instance.StartCoroutine(DissolveCoroutine(start, end, duration, customEventData.time, easing, track));
                 }
             }
         }
 
-        private static IEnumerator DissolveCoroutine(float cutoutStart, float cutoutEnd, float duration, float startTime,
-            List<CutoutAnimateEffect> cutoutAnimateEffects, Easings.Functions easing, Track track)
+        private static IEnumerator DissolveCoroutine(float cutoutStart, float cutoutEnd, float duration, float startTime, Easings.Functions easing, Track track)
         {
             float elapsedTime = 0f;
             while (elapsedTime < duration)
@@ -56,12 +43,31 @@ namespace NoodleExtensions.Animation
                 elapsedTime = _customEventCallbackController._audioTimeSource.songTime - startTime;
                 float time = elapsedTime / duration;
                 float cutout = 1 - Mathf.Lerp(cutoutStart, cutoutEnd, Easings.Interpolate(time, easing));
-                cutoutAnimateEffects.ForEach(n => n.SetCutout(cutout));
+                DissolveActiveObjects(cutout, track);
                 yield return null;
             }
-            cutoutAnimateEffects.ForEach(n => n.SetCutout(1 - cutoutEnd));
+            DissolveActiveObjects(1 - cutoutEnd, track);
             _activeCoroutines.Remove(track);
             yield break;
+        }
+
+        private static void DissolveActiveObjects(float time, Track track)
+        {
+            HashSet<CutoutAnimateEffect> cutoutAnimateEffects = new HashSet<CutoutAnimateEffect>();
+            foreach (NoteController noteController in GetActiveNotes(track))
+            {
+                BaseNoteVisuals baseNoteVisuals = noteController.gameObject.GetComponent<BaseNoteVisuals>();
+                cutoutAnimateEffects.Add(_noteCutoutAnimateEffectAccessor(ref baseNoteVisuals));
+            }
+            foreach (ObstacleController obstacleController in GetActiveObstacles(track))
+            {
+                ObstacleDissolve obstacleDissolve = obstacleController.gameObject.GetComponent<ObstacleDissolve>();
+                cutoutAnimateEffects.Add(_obstacleCutoutAnimateEffectAccessor(ref obstacleDissolve));
+            }
+            foreach(CutoutAnimateEffect cutoutAnimateEffect in cutoutAnimateEffects)
+            {
+                cutoutAnimateEffect.SetCutout(time);
+            }
         }
     }
 }
--- NoodleExtensions/NoodleExtensions/Animation/Events/DissolveArrow.cs
+++ NoodleExtensions/NoodleExtensions/Animation/Events/DissolveArrow.cs
@@ -26,21 +26,13 @@ namespace NoodleExtensions.Animation
                     string easingString = Trees.at(customEventData.data, EASING);
                     Easings.Functions easing = Easings.InterprateString(easingString);
 
-                    List<DisappearingArrowController> disappearingArrowControllers = new List<DisappearingArrowController>();
-                    foreach (NoteController noteController in GetActiveNotes(track))
-                    {
-                        DisappearingArrowController disappearingArrowController = noteController.gameObject.GetComponent<DisappearingArrowController>();
-                        disappearingArrowControllers.Add(disappearingArrowController);
-                    }
-
                     if (_activeCoroutines.TryGetValue(track, out Coroutine coroutine) && coroutine != null) _instance.StopCoroutine(coroutine);
-                    _activeCoroutines[track] = _instance.StartCoroutine(DissolveArrowCoroutine(start, end, duration, customEventData.time, disappearingArrowControllers, easing, track));
+                    _activeCoroutines[track] = _instance.StartCoroutine(DissolveArrowCoroutine(start, end, duration, customEventData.time, easing, track));
                 }
             }
         }
 
-        private static IEnumerator DissolveArrowCoroutine(float cutoutStart, float cutoutEnd, float duration, float startTime,
-            List<DisappearingArrowController> disappearingArrowControllers, Easings.Functions easing, Track track)
+        private static IEnumerator DissolveArrowCoroutine(float cutoutStart, float cutoutEnd, float duration, float startTime, Easings.Functions easing, Track track)
         {
             float elapsedTime = 0f;
             while (elapsedTime < duration)
@@ -48,12 +40,26 @@ namespace NoodleExtensions.Animation
                 elapsedTime = _customEventCallbackController._audioTimeSource.songTime - startTime;
                 float time = elapsedTime / duration;
                 float cutout = Mathf.Lerp(cutoutStart, cutoutEnd, Easings.Interpolate(time, easing));
-                disappearingArrowControllers.ForEach(n => n.SetArrowTransparency(cutout));
+                DissolveActiveArrow(cutout, track);
                 yield return null;
             }
-            disappearingArrowControllers.ForEach(n => n.SetArrowTransparency(cutoutEnd));
+            DissolveActiveArrow(cutoutEnd, track);
             _activeCoroutines.Remove(track);
             yield break;
+        }
+
+        private static void DissolveActiveArrow(float time, Track track)
+        {
+            HashSet<DisappearingArrowController> disappearingArrowControllers = new HashSet<DisappearingArrowController>();
+            foreach (NoteController noteController in GetActiveNotes(track))
+            {
+                DisappearingArrowController disappearingArrowController = noteController.gameObject.GetComponent<DisappearingArrowController>();
+                disappearingArrowControllers.Add(disappearingArrowController);
+            }
+            foreach(DisappearingArrowController disappearingArrowController in disappearingArrowControllers)
+            {
+                disappearingArrowController.SetArrowTransparency(time);
+            }
         }
     }
 }
