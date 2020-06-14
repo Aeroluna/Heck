62f9b5a176445c93597be3e608c014c8851a654d
Aeroluna <aeroluna5@gmail.com>
2020-06-14 14:25:11 -0700

    callback fix

--- NoodleExtensions/NoodleExtensions/HarmonyPatches/BeatmapObjectCallBackController.cs
+++ NoodleExtensions/NoodleExtensions/HarmonyPatches/BeatmapObjectCallBackController.cs
@@ -12,7 +12,8 @@ namespace NoodleExtensions.HarmonyPatches
     [NoodlePatch("LateUpdate")]
     internal class BeatmapObjectCallBackControllerLateUpdate
     {
-        private static readonly MethodInfo _getAheadTime = SymbolExtensions.GetMethodInfo(() => GetAheadTime(null, 0));
+        private static readonly MethodInfo _getAheadTime = SymbolExtensions.GetMethodInfo(() => GetAheadTime(null, null, 0));
+        private static readonly MethodInfo _beatmapObjectSpawnControllerCallback = typeof(BeatmapObjectSpawnController).GetMethod("HandleBeatmapObjectCallback", BindingFlags.Public | BindingFlags.Instance);
 
         private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
@@ -29,15 +30,17 @@ namespace NoodleExtensions.HarmonyPatches
 
                     instructionList.Insert(i + 1, new CodeInstruction(OpCodes.Call, _getAheadTime));
                     instructionList.Insert(i - 1, new CodeInstruction(OpCodes.Ldloc_3));
+                    instructionList.Insert(i - 1, new CodeInstruction(OpCodes.Ldloc_1));
                 }
             }
             if (!foundAheadTime) Logger.Log("Failed to find aheadTime ldfld, ping Aeroluna!", IPA.Logging.Logger.Level.Error);
             return instructionList.AsEnumerable();
         }
 
-        private static float GetAheadTime(BeatmapObjectData beatmapObjectData, float @default)
+        private static float GetAheadTime(BeatmapObjectCallbackController.BeatmapObjectCallbackData beatmapObjectCallbackData, BeatmapObjectData beatmapObjectData, float @default)
         {
-            if (beatmapObjectData is CustomObstacleData || beatmapObjectData is CustomNoteData)
+            if (beatmapObjectCallbackData.callback.Method == _beatmapObjectSpawnControllerCallback &&
+                (beatmapObjectData is CustomObstacleData || beatmapObjectData is CustomNoteData))
             {
                 dynamic dynData = ((dynamic)beatmapObjectData).customData;
                 float? aheadTime = (float?)Trees.at(dynData, "aheadTime");
