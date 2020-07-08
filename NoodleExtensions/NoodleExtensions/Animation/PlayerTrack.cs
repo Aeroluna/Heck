163eb4d65706fe8d4a244d2acd14a640d8ee4206
Reaxt <24214198+Reaxt@users.noreply.github.com>
2020-07-08 19:35:59 -0400

    Fixed room offset
    Player movement would reset room offset. This should fix that.

--- NoodleExtensions/NoodleExtensions/Animation/PlayerTrack.cs
+++ NoodleExtensions/NoodleExtensions/Animation/PlayerTrack.cs
@@ -10,6 +10,8 @@
         private static readonly FieldAccessor<PauseController, bool>.Accessor PauseBool = FieldAccessor<PauseController, bool>.GetAccessor("_paused");
         private static PlayerTrack _instance;
         private static Track _track;
+        private static Vector3 _startPos;
+        private static Quaternion _startRot;
         private static GameObject _origin;
         private static PauseController _pauseController;
 
@@ -20,6 +22,8 @@
                 _origin = GameObject.Find("GameCore/Origin");
                 _instance = _origin.AddComponent<PlayerTrack>();
                 _pauseController = FindObjectOfType<PauseController>();
+                _startRot = _origin.transform.localRotation;
+                _startPos = _origin.transform.localPosition;
             }
 
             _track = track;
@@ -33,21 +37,22 @@
             object position = _track.Properties[POSITION].Value;
             if (position != null && !paused)
             {
-                transform.localPosition = (Vector3)position * NoteLinesDistance;
+
+                transform.localPosition = ((Vector3)position * NoteLinesDistance) + _startPos;
             }
             else
             {
-                transform.localPosition = _vectorZero;
+                transform.localPosition = _startPos;
             }
 
             object rotation = _track.Properties[ROTATION].Value;
             if (rotation != null && !paused)
             {
-                transform.localRotation = (Quaternion)rotation;
+                transform.localRotation = ((Quaternion)rotation) * _startRot;
             }
             else
             {
-                transform.localRotation = _quaternionIdentity;
+                transform.localRotation = _startRot;
             }
         }
     }
