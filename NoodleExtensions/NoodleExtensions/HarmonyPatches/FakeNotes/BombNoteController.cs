f01ea79b4549c06342b2a068d9635754565ca5a5
Aeroluna <aeroluna5@gmail.com>
2020-07-22 21:52:12 -0700

    fix the last commit

--- NoodleExtensions/NoodleExtensions/HarmonyPatches/FakeNotes/BombNoteController.cs
+++ NoodleExtensions/NoodleExtensions/HarmonyPatches/FakeNotes/BombNoteController.cs
@@ -8,12 +8,12 @@
     {
         [HarmonyPriority(Priority.High)]
 #pragma warning disable SA1313
-        private static void Postfix(NoteData noteData, CuttableBySaber cuttableBySaber)
+        private static void Postfix(NoteData noteData, CuttableBySaber ____cuttableBySaber)
 #pragma warning restore SA1313
         {
             if (!FakeNoteHelper.GetCuttable(noteData))
             {
-                cuttableBySaber.canBeCut = false;
+                ____cuttableBySaber.canBeCut = false;
             }
         }
     }
--- NoodleExtensions/NoodleExtensions/HarmonyPatches/FakeNotes/GameNoteController.cs
+++ NoodleExtensions/NoodleExtensions/HarmonyPatches/FakeNotes/GameNoteController.cs
@@ -8,10 +8,10 @@
     {
         [HarmonyPriority(Priority.High)]
 #pragma warning disable SA1313
-        private static bool Prefix(GameNoteController noteController)
+        private static bool Prefix(GameNoteController __instance)
 #pragma warning restore SA1313
         {
-            return FakeNoteHelper.GetCuttable(noteController.noteData);
+            return FakeNoteHelper.GetCuttable(__instance.noteData);
         }
     }
 }
