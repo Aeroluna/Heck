13d8edfa3e5c65d28a43f9edd68df989cd9ea13f
Aeroluna <aeroluna5@gmail.com>
2020-10-22 12:26:16 -0700

    versioning and fix fake notes

--- NoodleExtensions/NoodleExtensions/HarmonyPatches/FakeNotes/BeatmapObjectManager.cs
+++ NoodleExtensions/NoodleExtensions/HarmonyPatches/FakeNotes/BeatmapObjectManager.cs
@@ -1,15 +1,37 @@
 ﻿namespace NoodleExtensions.HarmonyPatches
 {
+    using System;
+    using System.Linq;
+    using System.Reflection;
     using HarmonyLib;
+    using UnityEngine;
 
     [NoodlePatch(typeof(BeatmapObjectManager))]
     [NoodlePatch("HandleNoteWasCut")]
     internal static class BeatmapObjectManagerHandleNoteWasCut
     {
+        private static readonly MethodInfo _despawnMethod = typeof(BeatmapObjectManager).GetMethod("Despawn", BindingFlags.Instance | BindingFlags.NonPublic, null, new Type[] { typeof(NoteController) }, null);
+        private static NoteCutCoreEffectsSpawner _noteCutCoreEffectsSpawner;
+
         [HarmonyPriority(Priority.High)]
-        private static bool Prefix(NoteController noteController)
+#pragma warning disable SA1313 // Parameter names should begin with lower-case letter
+        private static bool Prefix(BeatmapObjectManager __instance, NoteController noteController, NoteCutInfo noteCutInfo)
+#pragma warning restore SA1313 // Parameter names should begin with lower-case letter
         {
-            return FakeNoteHelper.GetFakeNote(noteController);
+            if (!FakeNoteHelper.GetFakeNote(noteController))
+            {
+                if (_noteCutCoreEffectsSpawner == null)
+                {
+                    _noteCutCoreEffectsSpawner = Resources.FindObjectsOfTypeAll<NoteCutCoreEffectsSpawner>().First();
+                }
+
+                _noteCutCoreEffectsSpawner.HandleNoteWasCut(noteController, noteCutInfo);
+                _despawnMethod.Invoke(__instance, new object[] { noteController });
+
+                return false;
+            }
+
+            return true;
         }
     }
 
