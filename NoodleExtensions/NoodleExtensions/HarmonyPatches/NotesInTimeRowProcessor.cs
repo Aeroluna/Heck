318067e9818078b122858c716c890e1acce99c10
Aeroluna <aeroluna5@gmail.com>
2021-02-24 20:16:13 -0700

    tutorial fix

--- NoodleExtensions/NoodleExtensions/HarmonyPatches/NotesInTimeRowProcessor.cs
+++ NoodleExtensions/NoodleExtensions/HarmonyPatches/NotesInTimeRowProcessor.cs
@@ -15,76 +15,79 @@
 
         private static void Postfix(List<NoteData> notes)
         {
-            List<CustomNoteData> customNotes = notes.Cast<CustomNoteData>().ToList();
-
-            _notesInColumns.Clear();
-            for (int i = 0; i < customNotes.Count; i++)
+            if (notes.FirstOrDefault() is CustomNoteData)
             {
-                CustomNoteData noteData = customNotes[i];
+                List<CustomNoteData> customNotes = notes.Cast<CustomNoteData>().ToList();
 
-                IEnumerable<float?> position = ((List<object>)Trees.at(noteData.customData, POSITION))?.Select(n => n.ToNullableFloat());
-                float lineIndex = position?.ElementAtOrDefault(0) ?? (noteData.lineIndex - 2);
-                float lineLayer = position?.ElementAtOrDefault(1) ?? (float)noteData.noteLineLayer;
-
-                if (!_notesInColumns.TryGetValue(lineIndex, out List<CustomNoteData> list))
+                _notesInColumns.Clear();
+                for (int i = 0; i < customNotes.Count; i++)
                 {
-                    list = new List<CustomNoteData>();
-                    _notesInColumns.Add(lineIndex, list);
-                }
+                    CustomNoteData noteData = customNotes[i];
 
-                bool flag = false;
-                for (int k = 0; k < list.Count; k++)
-                {
-                    IEnumerable<float?> listPosition = ((List<object>)Trees.at(list[k].customData, POSITION))?.Select(n => n.ToNullableFloat());
-                    float listLineLayer = listPosition?.ElementAtOrDefault(1) ?? (float)list[k].noteLineLayer;
-                    if (listLineLayer > lineLayer)
+                    IEnumerable<float?> position = ((List<object>)Trees.at(noteData.customData, POSITION))?.Select(n => n.ToNullableFloat());
+                    float lineIndex = position?.ElementAtOrDefault(0) ?? (noteData.lineIndex - 2);
+                    float lineLayer = position?.ElementAtOrDefault(1) ?? (float)noteData.noteLineLayer;
+
+                    if (!_notesInColumns.TryGetValue(lineIndex, out List<CustomNoteData> list))
                     {
-                        list.Insert(k, noteData);
-                        flag = true;
-                        break;
+                        list = new List<CustomNoteData>();
+                        _notesInColumns.Add(lineIndex, list);
+                    }
+
+                    bool flag = false;
+                    for (int k = 0; k < list.Count; k++)
+                    {
+                        IEnumerable<float?> listPosition = ((List<object>)Trees.at(list[k].customData, POSITION))?.Select(n => n.ToNullableFloat());
+                        float listLineLayer = listPosition?.ElementAtOrDefault(1) ?? (float)list[k].noteLineLayer;
+                        if (listLineLayer > lineLayer)
+                        {
+                            list.Insert(k, noteData);
+                            flag = true;
+                            break;
+                        }
+                    }
+
+                    if (!flag)
+                    {
+                        list.Add(noteData);
                     }
                 }
 
-                if (!flag)
+                foreach (KeyValuePair<float, List<CustomNoteData>> keyValue in _notesInColumns)
                 {
-                    list.Add(noteData);
-                }
-            }
-
-            foreach (KeyValuePair<float, List<CustomNoteData>> keyValue in _notesInColumns)
-            {
-                List<CustomNoteData> list2 = keyValue.Value;
-                for (int m = 0; m < list2.Count; m++)
-                {
-                    list2[m].customData.startNoteLineLayer = m;
-                }
-            }
-
-            // Process flip data
-            List<CustomNoteData> flipNotes = new List<CustomNoteData>(customNotes);
-            for (int i = flipNotes.Count - 1; i >= 0; i--)
-            {
-                dynamic dynData = flipNotes[i].customData;
-                IEnumerable<float?> flip = ((List<object>)Trees.at(dynData, FLIP))?.Select(n => n.ToNullableFloat());
-                float? flipX = flip?.ElementAtOrDefault(0);
-                float? flipY = flip?.ElementAtOrDefault(1);
-                if (flipX.HasValue || flipY.HasValue)
-                {
-                    if (flipX.HasValue)
+                    List<CustomNoteData> list2 = keyValue.Value;
+                    for (int m = 0; m < list2.Count; m++)
                     {
-                        dynData.flipLineIndex = flipX.Value;
+                        list2[m].customData.startNoteLineLayer = m;
                     }
-
-                    if (flipY.HasValue)
-                    {
-                        dynData.flipYSide = flipY.Value;
-                    }
-
-                    flipNotes.Remove(flipNotes[i]);
                 }
-            }
 
-            flipNotes.ForEach(c => c.customData.flipYSide = 0);
+                // Process flip data
+                List<CustomNoteData> flipNotes = new List<CustomNoteData>(customNotes);
+                for (int i = flipNotes.Count - 1; i >= 0; i--)
+                {
+                    dynamic dynData = flipNotes[i].customData;
+                    IEnumerable<float?> flip = ((List<object>)Trees.at(dynData, FLIP))?.Select(n => n.ToNullableFloat());
+                    float? flipX = flip?.ElementAtOrDefault(0);
+                    float? flipY = flip?.ElementAtOrDefault(1);
+                    if (flipX.HasValue || flipY.HasValue)
+                    {
+                        if (flipX.HasValue)
+                        {
+                            dynData.flipLineIndex = flipX.Value;
+                        }
+
+                        if (flipY.HasValue)
+                        {
+                            dynData.flipYSide = flipY.Value;
+                        }
+
+                        flipNotes.Remove(flipNotes[i]);
+                    }
+                }
+
+                flipNotes.ForEach(c => c.customData.flipYSide = 0);
+            }
         }
     }
 
@@ -94,38 +97,41 @@
     {
         private static void Postfix(List<NoteData> colorNotesData)
         {
-            List<CustomNoteData> customNotes = colorNotesData.Cast<CustomNoteData>().ToList();
-
-            int customNoteCount = customNotes.Count;
-            if (customNoteCount == 2)
+            if (colorNotesData.FirstOrDefault() is CustomNoteData)
             {
-                float[] lineIndexes = new float[2];
-                float[] lineLayers = new float[2];
-                for (int i = 0; i < customNoteCount; i++)
-                {
-                    dynamic dynData = customNotes[i].customData;
-                    IEnumerable<float?> position = ((List<object>)Trees.at(dynData, POSITION))?.Select(n => n.ToNullableFloat());
-                    lineIndexes[i] = position?.ElementAtOrDefault(0) ?? (customNotes[i].lineIndex - 2);
-                    lineLayers[i] = position?.ElementAtOrDefault(1) ?? (float)customNotes[i].noteLineLayer;
-                }
+                List<CustomNoteData> customNotes = colorNotesData.Cast<CustomNoteData>().ToList();
 
-                if (customNotes[0].colorType != customNotes[1].colorType && ((customNotes[0].colorType == ColorType.ColorA && lineIndexes[0] > lineIndexes[1]) ||
-                    (customNotes[0].colorType == ColorType.ColorB && lineIndexes[0] < lineIndexes[1])))
+                int customNoteCount = customNotes.Count;
+                if (customNoteCount == 2)
                 {
+                    float[] lineIndexes = new float[2];
+                    float[] lineLayers = new float[2];
                     for (int i = 0; i < customNoteCount; i++)
                     {
-                        // apparently I can use customData to store my own variables in noteData, neat
                         dynamic dynData = customNotes[i].customData;
-                        dynData.flipLineIndex = lineIndexes[1 - i];
+                        IEnumerable<float?> position = ((List<object>)Trees.at(dynData, POSITION))?.Select(n => n.ToNullableFloat());
+                        lineIndexes[i] = position?.ElementAtOrDefault(0) ?? (customNotes[i].lineIndex - 2);
+                        lineLayers[i] = position?.ElementAtOrDefault(1) ?? (float)customNotes[i].noteLineLayer;
+                    }
 
-                        float flipYSide = (lineIndexes[i] > lineIndexes[1 - i]) ? 1 : -1;
-                        if ((lineIndexes[i] > lineIndexes[1 - i] && lineLayers[i] < lineLayers[1 - i]) || (lineIndexes[i] < lineIndexes[1 - i] &&
-                            lineLayers[i] > lineLayers[1 - i]))
+                    if (customNotes[0].colorType != customNotes[1].colorType && ((customNotes[0].colorType == ColorType.ColorA && lineIndexes[0] > lineIndexes[1]) ||
+                        (customNotes[0].colorType == ColorType.ColorB && lineIndexes[0] < lineIndexes[1])))
+                    {
+                        for (int i = 0; i < customNoteCount; i++)
                         {
-                            flipYSide *= -1f;
-                        }
+                            // apparently I can use customData to store my own variables in noteData, neat
+                            dynamic dynData = customNotes[i].customData;
+                            dynData.flipLineIndex = lineIndexes[1 - i];
 
-                        dynData.flipYSide = flipYSide;
+                            float flipYSide = (lineIndexes[i] > lineIndexes[1 - i]) ? 1 : -1;
+                            if ((lineIndexes[i] > lineIndexes[1 - i] && lineLayers[i] < lineLayers[1 - i]) || (lineIndexes[i] < lineIndexes[1 - i] &&
+                                lineLayers[i] > lineLayers[1 - i]))
+                            {
+                                flipYSide *= -1f;
+                            }
+
+                            dynData.flipYSide = flipYSide;
+                        }
                     }
                 }
             }
