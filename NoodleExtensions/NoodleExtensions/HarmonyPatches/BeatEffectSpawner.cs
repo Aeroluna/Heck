559c57aed158bfa0ec9201873bc7503e503b4c12
Aeroluna <aeroluna5@gmail.com>
2021-03-29 11:37:23 -0700

    BeatEffect fix

---
+++ NoodleExtensions/NoodleExtensions/HarmonyPatches/BeatEffectSpawner.cs
@@ -0,0 +1,71 @@
+﻿namespace NoodleExtensions.HarmonyPatches
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Reflection.Emit;
+    using HarmonyLib;
+    using UnityEngine;
+
+    [NoodlePatch(typeof(BeatEffectSpawner))]
+    [NoodlePatch("HandleNoteDidStartJump")]
+    internal static class BeatEffectSpawnerHandleNoteDidStartJump
+    {
+        private static readonly MethodInfo _getNoteControllerPosition = SymbolExtensions.GetMethodInfo(() => GetNoteControllerPosition(null));
+        private static readonly MethodInfo _getNoteControllerRotation = SymbolExtensions.GetMethodInfo(() => GetNoteControllerRotation(null));
+
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            List<CodeInstruction> instructionList = instructions.ToList();
+            bool foundInit = false;
+            bool foundJumpStartPos = false;
+            for (int i = 0; i < instructionList.Count; i++)
+            {
+                if (!foundJumpStartPos &&
+                    instructionList[i].opcode == OpCodes.Callvirt &&
+                    ((MethodInfo)instructionList[i].operand).Name == "get_jumpStartPos")
+                {
+                    foundJumpStartPos = true;
+
+                    instructionList.RemoveRange(i - 2, 4);
+                    instructionList.Insert(i - 2, new CodeInstruction(OpCodes.Call, _getNoteControllerPosition));
+                }
+            }
+
+            for (int i = 0; i < instructionList.Count; i++)
+            {
+                if (!foundInit &&
+                    instructionList[i].opcode == OpCodes.Callvirt &&
+                    ((MethodInfo)instructionList[i].operand).Name == "Init")
+                {
+                    foundInit = true;
+
+                    instructionList[i - 1] = new CodeInstruction(OpCodes.Call, _getNoteControllerRotation);
+                }
+            }
+
+            if (!foundJumpStartPos)
+            {
+                NoodleLogger.Log("Failed to find callvirt to get_jumpStartPos!", IPA.Logging.Logger.Level.Error);
+            }
+
+            if (!foundInit)
+            {
+                NoodleLogger.Log("Failed to find callvirt to Init!", IPA.Logging.Logger.Level.Error);
+            }
+
+            return instructionList.AsEnumerable();
+        }
+
+        private static Vector3 GetNoteControllerPosition(NoteController noteController)
+        {
+            return noteController.transform.position;
+        }
+
+        private static Quaternion GetNoteControllerRotation(NoteController noteController)
+        {
+            return noteController.transform.rotation;
+        }
+    }
+}
