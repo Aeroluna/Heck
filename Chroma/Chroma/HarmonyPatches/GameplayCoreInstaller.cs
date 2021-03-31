a3e2936c1edf8587225dd0bd0299af7e8e560599
Aeroluna <aeroluna5@gmail.com>
2021-03-30 21:57:49 -0700

    reloader compatibility

--- Chroma/Chroma/HarmonyPatches/GameplayCoreInstaller.cs
+++ Chroma/Chroma/HarmonyPatches/GameplayCoreInstaller.cs
@@ -1,48 +1,22 @@
-﻿namespace NoodleExtensions.HarmonyPatches
+﻿namespace Chroma.HarmonyPatches
 {
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Reflection;
-    using System.Reflection.Emit;
+    using System.Diagnostics;
     using Chroma;
     using HarmonyLib;
 
-    // We do all this because we only want this to run on the local player
-    [HarmonyPatch(typeof(GameplayCoreInstaller))]
-    [HarmonyPatch("InstallBindings")]
+    [HarmonyPatch(typeof(BeatmapDataTransformHelper))]
+    [HarmonyPatch("CreateTransformedBeatmapData")]
     internal static class GameplayCoreInstallerInstallBindings
     {
-        private static readonly MethodInfo _readCustomData = SymbolExtensions.GetMethodInfo(() => ReadCustomData(null));
-
-        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        private static void Postfix(IReadonlyBeatmapData __result)
         {
-            List<CodeInstruction> instructionList = instructions.ToList();
-            bool foundBeatmapData = false;
-            for (int i = 0; i < instructionList.Count; i++)
+            // Skip if calling class is MultiplayerConnectPlayerInstaller
+            StackTrace stackTrace = new StackTrace();
+            if (!stackTrace.GetFrame(2).GetMethod().Name.Contains("MultiplayerConnectedPlayerInstaller"))
             {
-                if (!foundBeatmapData &&
-                    instructionList[i].opcode == OpCodes.Call &&
-                    ((MethodInfo)instructionList[i].operand).Name == "CreateTransformedBeatmapData")
-                {
-                    foundBeatmapData = true;
-
-                    instructionList.Insert(i + 1, new CodeInstruction(OpCodes.Call, _readCustomData));
-                }
+                ChromaObjectDataManager.DeserializeBeatmapData(__result);
+                ChromaEventDataManager.DeserializeBeatmapData(__result);
             }
-
-            if (!foundBeatmapData)
-            {
-                ChromaLogger.Log("Failed to find Call to CreateTransformedBeatmapData!", IPA.Logging.Logger.Level.Error);
-            }
-
-            return instructionList.AsEnumerable();
-        }
-
-        private static IReadonlyBeatmapData ReadCustomData(IReadonlyBeatmapData result)
-        {
-            ChromaObjectDataManager.DeserializeBeatmapData(result);
-            ChromaEventDataManager.DeserializeBeatmapData(result);
-            return result;
         }
     }
 }
