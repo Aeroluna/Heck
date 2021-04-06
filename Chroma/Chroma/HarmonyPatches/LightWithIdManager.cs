1562d142155dccbb01b4648fef3c51ab9d5ac67d
Aeroluna <aeroluna5@gmail.com>
2021-04-06 04:56:06 -0700

    fps improvements when disabling many lights

---
+++ Chroma/Chroma/HarmonyPatches/LightWithIdManager.cs
@@ -0,0 +1,16 @@
+﻿namespace Chroma.HarmonyPatches
+{
+    using System.Collections.Generic;
+
+    [ChromaPatch(typeof(LightWithIdManager))]
+    [ChromaPatch("LateUpdate")]
+    internal static class LightWithIdManagerLateUpdate
+    {
+        private static void Postfix(List<ILightWithId> ____lightsToUnregister)
+        {
+            // For some reason this doesnt get emptied and is continuously looped over.
+            // When unregistering a large amount of lights in Chroma, this can add lag, so we dump the list ourselves.
+            ____lightsToUnregister.Clear();
+        }
+    }
+}
