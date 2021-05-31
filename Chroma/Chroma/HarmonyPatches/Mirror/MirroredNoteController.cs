cb2b5947f2e24a272cd413651121f8a5e2123a16
Aeroluna <aeroluna5@gmail.com>
2021-05-31 15:49:27 -0700

    color reflections

---
+++ Chroma/Chroma/HarmonyPatches/Mirror/MirroredNoteController.cs
@@ -0,0 +1,47 @@
+﻿namespace Chroma.HarmonyPatches
+{
+    using Chroma.Colorizer;
+    using Heck;
+
+    internal static class MirroredNoteControllerHelper
+    {
+        internal static void UpdateMirror(NoteControllerBase noteController, NoteControllerBase followedNote)
+        {
+            if (noteController is MirroredBombNoteController)
+            {
+                if (followedNote.TryGetBombColorizer(out BombColorizer bombColorizer))
+                {
+                    noteController.ColorizeBomb(bombColorizer.Color);
+                }
+            }
+            else
+            {
+                if (followedNote.TryGetNoteColorizer(out NoteColorizer noteColorizer))
+                {
+                    noteController.ColorizeNote(noteColorizer.Color);
+                }
+            }
+        }
+    }
+
+    // Fuck generics
+    [HeckPatch(typeof(MirroredNoteController<INoteMirrorable>))]
+    [HeckPatch("UpdatePositionAndRotation")]
+    internal static class MirroredNoteControllerINoteMirrorableUpdatePositionAndRotation
+    {
+        private static void Postfix(MirroredNoteController<INoteMirrorable> __instance, INoteMirrorable ___followedNote)
+        {
+            MirroredNoteControllerHelper.UpdateMirror(__instance, (NoteControllerBase)___followedNote);
+        }
+    }
+
+    [HeckPatch(typeof(MirroredNoteController<ICubeNoteMirrorable>))]
+    [HeckPatch("UpdatePositionAndRotation")]
+    internal static class MirroredNoteControllerICubeNoteMirrorableUpdatePositionAndRotation
+    {
+        private static void Postfix(MirroredNoteController<ICubeNoteMirrorable> __instance, ICubeNoteMirrorable ___followedNote)
+        {
+            MirroredNoteControllerHelper.UpdateMirror(__instance, (NoteControllerBase)___followedNote);
+        }
+    }
+}
---
+++ Chroma/Chroma/HarmonyPatches/Mirror/MirroredObstacleController.cs
@@ -0,0 +1,18 @@
+﻿namespace Chroma.HarmonyPatches
+{
+    using Chroma.Colorizer;
+    using Heck;
+
+    [HeckPatch(typeof(MirroredObstacleController))]
+    [HeckPatch("UpdatePositionAndRotation")]
+    internal static class MirroredObstacleControllerUpdatePositionAndRotation
+    {
+        private static void Postfix(MirroredObstacleController __instance, ObstacleController ____followedObstacle)
+        {
+            if (____followedObstacle.TryGetObstacleColorizer(out ObstacleColorizer obstacleColorizer))
+            {
+                __instance.ColorizeObstacle(obstacleColorizer.Color);
+            }
+        }
+    }
+}
