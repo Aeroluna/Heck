2593d85e11d82df1ca92a5b86ea9a5aa0c5fd135
Aeroluna <aeroluna5@gmail.com>
2021-05-31 19:42:11 -0700

    Moved to heck

--- NoodleExtensions/NoodleExtensions/HarmonyPatches/Mirror/MirroredObstacleController.cs
+++ NoodleExtensions/NoodleExtensions/HarmonyPatches/Mirror/MirroredObstacleController.cs
@@ -9,48 +9,11 @@
     using UnityEngine;
 
     [HeckPatch(typeof(MirroredObstacleController))]
-    [HeckPatch("Mirror")]
-    internal static class MirroredObstacleControllerAwake
-    {
-        private static readonly FieldInfo _followedObstacleField = AccessTools.Field(typeof(MirroredObstacleController), "_followedObstacle");
-
-        // Looks like you forgot to fill _followedObstacle beat games, i got you covered!
-        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-        {
-            List<CodeInstruction> instructionList = instructions.ToList();
-            bool foundRemoveListeners = false;
-            for (int i = 0; i < instructionList.Count; i++)
-            {
-                if (!foundRemoveListeners &&
-                    instructionList[i].opcode == OpCodes.Call &&
-                    ((MethodInfo)instructionList[i].operand).Name == "RemoveListeners")
-                {
-                    foundRemoveListeners = true;
-                    CodeInstruction[] codeInstructions = new CodeInstruction[]
-                    {
-                        new CodeInstruction(OpCodes.Ldarg_0),
-                        new CodeInstruction(OpCodes.Ldarg_1),
-                        new CodeInstruction(OpCodes.Stfld, _followedObstacleField),
-                    };
-                    instructionList.InsertRange(i + 1, codeInstructions);
-                }
-            }
-
-            if (!foundRemoveListeners)
-            {
-                Plugin.Logger.Log("Failed to find call to RemoveListeners!", IPA.Logging.Logger.Level.Error);
-            }
-
-            return instructionList.AsEnumerable();
-        }
-    }
-
-    [HeckPatch(typeof(MirroredObstacleController))]
     [HeckPatch("UpdatePositionAndRotation")]
     internal static class MirroredObstacleControllerUpdatePositionAndRotation
     {
         // Must be overwritten to compensate for rotation
-        private static bool Prefix(MirroredObstacleController __instance, ObstacleController ____followedObstacle, Transform ____transform, Transform ____followedTransform)
+        private static void Postfix(MirroredObstacleController __instance, ObstacleController ____followedObstacle, Transform ____transform, Transform ____followedTransform)
         {
             Vector3 position = ____followedTransform.position;
             Quaternion quaternion = ____followedTransform.rotation;
@@ -70,8 +33,6 @@
                     cutoutAnimateEffect.SetCutout(followedCutoutAnimateEffect.Cutout);
                 }
             }
-
-            return false;
         }
     }
 }
