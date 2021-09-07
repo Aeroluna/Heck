9f6a31d2baf47b446763079e615ea3f118831c91
Aeroluna <aeroluna5@gmail.com>
2021-09-07 15:24:27 -0700

    heckpatch id

--- Heck/HeckPatch.cs
+++ Heck/HeckPatch.cs
@@ -2,12 +2,13 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Reflection;
     using HarmonyLib;
 
     internal struct HeckPatchData
     {
-        internal HeckPatchData(MethodBase orig, MethodInfo pre, MethodInfo post, MethodInfo tran)
+        internal HeckPatchData(MethodBase orig, MethodInfo? pre, MethodInfo? post, MethodInfo? tran)
         {
             OriginalMethod = orig;
             Prefix = pre;
@@ -17,11 +18,11 @@
 
         internal MethodBase OriginalMethod { get; }
 
-        internal MethodInfo Prefix { get; }
+        internal MethodInfo? Prefix { get; }
 
-        internal MethodInfo Postfix { get; }
+        internal MethodInfo? Postfix { get; }
 
-        internal MethodInfo Transpiler { get; }
+        internal MethodInfo? Transpiler { get; }
     }
 
     public class HeckData
@@ -71,14 +72,21 @@
             }
             else
             {
-                Plugin.Logger.Log($"Could not find Heck patch data for {id}", IPA.Logging.Logger.Level.Critical);
+                Plugin.Logger.Log($"Could not find Heck patch data for {id}.", IPA.Logging.Logger.Level.Critical);
             }
         }
 
         public static void InitPatches(Harmony id, Assembly assembly)
         {
-            if (!_heckPatches.ContainsKey(id))
+            InitPatches(id, assembly, 0);
+        }
+
+        public static void InitPatches(Harmony harmony, Assembly assembly, int id)
+        {
+            if (!_heckPatches.ContainsKey(harmony))
             {
+                Plugin.Logger.Log($"Initializing patches for Harmony instance [{harmony.Id}] in [{assembly.GetName()}].", IPA.Logging.Logger.Level.Trace);
+
                 List<HeckPatchData> heckPatchDatas = new List<HeckPatchData>();
                 foreach (Type type in assembly.GetTypes())
                 {
@@ -97,6 +105,7 @@
                         List<string> methodNames = new List<string>();
                         Type[]? parameters = null;
                         MethodType methodType = MethodType.Normal;
+                        int patchId = 0;
                         foreach (HeckPatch n in attributes)
                         {
                             if (n.DeclaringType != null)
@@ -118,6 +127,21 @@
                             {
                                 methodType = n.MethodType.Value;
                             }
+
+                            if (n.MethodType != null)
+                            {
+                                methodType = n.MethodType.Value;
+                            }
+
+                            if (n.Id != null)
+                            {
+                                patchId = n.Id.Value;
+                            }
+                        }
+
+                        if (patchId != id)
+                        {
+                            continue;
                         }
 
                         if (declaringType == null)
@@ -125,9 +149,12 @@
                             throw new ArgumentException("Type not described");
                         }
 
-                        MethodInfo prefix = AccessTools.Method(type, "Prefix");
-                        MethodInfo postfix = AccessTools.Method(type, "Postfix");
-                        MethodInfo transpiler = AccessTools.Method(type, "Transpiler");
+                        MethodInfo? prefix = AccessTools.Method(type, "Prefix");
+                        MethodInfo? postfix = AccessTools.Method(type, "Postfix");
+                        MethodInfo? transpiler = AccessTools.Method(type, "Transpiler");
+
+                        // Logging
+                        string methodsContained = string.Join(", ", new MethodInfo[] { prefix, postfix, transpiler }.Where(n => n != null).Select(n => n.Name));
 
                         BindingFlags flags = BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.DeclaredOnly;
                         switch (methodType)
@@ -148,9 +175,10 @@
 
                                     if (normalMethodBase == null)
                                     {
-                                        throw new ArgumentException($"Could not find method '{methodName}' of '{declaringType}'");
+                                        throw new ArgumentException($"Could not find method '{methodName}' of '{declaringType}'.");
                                     }
 
+                                    Plugin.Logger.Log($"[{harmony.Id}] Found patch for method [{declaringType.FullName}.{normalMethodBase.Name}] containing [{methodsContained}]", IPA.Logging.Logger.Level.Trace);
                                     heckPatchDatas.Add(new HeckPatchData(normalMethodBase, prefix, postfix, transpiler));
                                 }
 
@@ -169,9 +197,10 @@
 
                                 if (constructorMethodBase == null)
                                 {
-                                    throw new ArgumentException($"Could not find constructor for '{declaringType}'");
+                                    throw new ArgumentException($"Could not find constructor for '{declaringType}'.");
                                 }
 
+                                Plugin.Logger.Log($"[{harmony.Id}] Found patch for constructor [{declaringType.FullName}.{constructorMethodBase.Name}] containing [{methodsContained}]", IPA.Logging.Logger.Level.Trace);
                                 heckPatchDatas.Add(new HeckPatchData(constructorMethodBase, prefix, postfix, transpiler));
 
                                 break;
@@ -182,11 +211,11 @@
                     }
                 }
 
-                _heckPatches.Add(id, new HeckData(heckPatchDatas));
+                _heckPatches.Add(harmony, new HeckData(heckPatchDatas));
             }
             else
             {
-                throw new ArgumentException($"Attempted to add duplicate entry {id}.", nameof(id));
+                throw new ArgumentException($"Attempted to add duplicate entry [{harmony.Id}].", nameof(harmony));
             }
         }
     }
@@ -214,6 +243,11 @@
             MethodType = methodType;
         }
 
+        public HeckPatch(int id)
+        {
+            Id = id;
+        }
+
         public HeckPatch(Type declaringType, string methodName)
         {
             DeclaringType = declaringType;
@@ -227,5 +261,7 @@
         internal Type[]? Parameters { get; }
 
         internal MethodType? MethodType { get; }
+
+        internal int? Id { get; }
     }
 }
