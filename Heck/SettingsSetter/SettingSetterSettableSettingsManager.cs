a1c33fe201546652a9a557e7d791a639c4ee14cb
Aeroluna <aeroluna5@gmail.com>
2021-09-15 19:30:53 -0700

    settablesetting and

--- Heck/SettingsSetter/SettingSetterSettableSettingsManager.cs
+++ Heck/SettingsSetter/SettingSetterSettableSettingsManager.cs
@@ -6,11 +6,29 @@
     using System.Reflection;
     using Newtonsoft.Json;
 
-    internal static class SettingSetterSettableSettingsManager
+    public static class SettingSetterSettableSettingsManager
     {
         private static Dictionary<string, List<Dictionary<string, object>>>? _settingsTable;
 
-        public static Dictionary<string, List<Dictionary<string, object>>> SettingsTable => _settingsTable ?? throw new InvalidOperationException($"[{nameof(_settingsTable)}] was not created.");
+        internal static Dictionary<string, List<Dictionary<string, object>>> SettingsTable => _settingsTable ?? throw new InvalidOperationException($"[{nameof(_settingsTable)}] was not created.");
+
+        internal static Dictionary<string, Dictionary<string, ISettableSetting>> SettableSettings { get; } = new Dictionary<string, Dictionary<string, ISettableSetting>>();
+
+        public static void RegisterSettableSetting(string group, string fieldName, ISettableSetting settableSetting)
+        {
+            if (!SettableSettings.TryGetValue(group, out Dictionary<string, ISettableSetting> groupSettings))
+            {
+                groupSettings = new Dictionary<string, ISettableSetting>();
+                SettableSettings[group] = groupSettings;
+            }
+
+            if (groupSettings.ContainsKey(fieldName))
+            {
+                throw new ArgumentException($"Group [{group}] already contains [{fieldName}].", nameof(fieldName));
+            }
+
+            groupSettings[fieldName] = settableSetting;
+        }
 
         internal static void SetupSettingsTable()
         {
