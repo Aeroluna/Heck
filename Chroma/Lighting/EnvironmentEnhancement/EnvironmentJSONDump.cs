ff048f9d8e4e8d87eb82aa34d2d3641e8deade90
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2022-02-08 18:00:35 -0400

    Environment JSON dump

---
+++ Chroma/Lighting/EnvironmentEnhancement/EnvironmentJSONDump.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+namespace Chroma.Lighting.EnvironmentEnhancement
+{
+    struct Vector3Json
+    {
+        public float x;
+        public float y;
+        public float z;
+
+        public Vector3Json(Vector3 vector3)
+        {
+            x = vector3.x;
+            y = vector3.y;
+            z = vector3.z;
+        }
+    }
+
+    struct QuaternionJson
+    {
+        public float x;
+        public float y;
+        public float z;
+        public float w;
+
+        public QuaternionJson(Quaternion quaternion)
+        {
+            x = quaternion.x;
+            y = quaternion.y;
+            z = quaternion.z;
+            w = quaternion.w;
+        }
+    }
+
+    struct GameObjectJSON
+    {
+        public readonly Vector3Json position;
+        public readonly Vector3Json localPosition;
+        public readonly QuaternionJson rotation;
+        public readonly QuaternionJson localRotation;
+        public readonly Vector3Json localScale;
+
+        public GameObjectJSON(GameObject gameObject)
+        {
+            Transform transform = gameObject.transform;
+            localPosition = new Vector3Json(transform.localPosition);
+            position = new Vector3Json(transform.position);
+            rotation = new QuaternionJson(transform.rotation);
+            localRotation = new QuaternionJson(transform.localRotation);
+            localScale = new Vector3Json(transform.localScale);
+        }
+    }
+}
