8dae8676c1df7f7016291b4258b195e55ce6ed8a
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2022-07-27 20:27:17 -0400

    custom mesh

---
+++ Chroma/Extras/CustomDataExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CustomJSONData;
+using CustomJSONData.CustomBeatmap;
+
+namespace Heck
+{
+    // TODO: Move to CustomJsonData
+    public static class CustomDataExtensions
+    {
+        public static IEnumerable<T>? GetFromArrayOfFloatArray<T>(this CustomData customData, string key, Func<float[], T> convert)
+        {
+            return customData.Get<IEnumerable<IEnumerable<object>>>(key)?.Select(o => convert(o.Select(Convert.ToSingle).ToArray()));
+        }
+
+        public static IEnumerable<T> GetRequiredFromArrayOfFloatArray<T>(this CustomData customData, string key, Func<float[], T> convert)
+        {
+            return customData.GetFromArrayOfFloatArray(key, convert) ?? throw new JsonNotDefinedException(key);
+        }
+    }
+}
