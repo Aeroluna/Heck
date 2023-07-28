149f1ef44daa0674f8e031f4b35cf33319d14c7b
Aeroluna <aeroluna5@gmail.com>
2023-07-27 21:12:56 -0700

    fix move towards head

---
+++ NoodleExtensions/HarmonyPatches/SmallFixes/PlayerTransformsHeadOffsetAdjust.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using Heck;
+using UnityEngine;
+
+namespace NoodleExtensions.HarmonyPatches.SmallFixes
+{
+    [HeckPatch(PatchType.Features)]
+    [HarmonyPatch(typeof(PlayerTransforms))]
+    internal static class PlayerTransformsHeadOffsetAdjust
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(PlayerTransforms.HeadOffsetZ))]
+        private static bool Prefix(
+            Quaternion noteInverseWorldRotation,
+            Vector3 ____headPseudoLocalPos,
+            Transform ____originParentTransform,
+            ref float __result)
+        {
+            // get magnitude in direction we care about rather than just z
+            __result = Vector3.Dot(noteInverseWorldRotation * ____headPseudoLocalPos, ____originParentTransform.forward);
+            return false;
+        }
+    }
+}
