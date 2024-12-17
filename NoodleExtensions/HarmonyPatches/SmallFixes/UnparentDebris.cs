c75f61df49006e2331b371b3700a645ca3928615
Aeroluna <40481393+Aeroluna@users.noreply.github.com>
2024-12-17 16:40:41 -0700

    force unparent debris

---
+++ NoodleExtensions/HarmonyPatches/SmallFixes/UnparentDebris.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using Heck;
+
+namespace NoodleExtensions.HarmonyPatches.SmallFixes;
+
+[HeckPatch(PatchType.Features)]
+internal static class UnparentDebris
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(NoteDebris), nameof(NoteDebris.Init))]
+    private static void Unparent(NoteDebris __instance)
+    {
+        __instance.transform.SetParent(null, true);
+    }
+}
