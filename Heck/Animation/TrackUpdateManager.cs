cf57176f151818566791f33fb62c82407a926136
Aeroluna <40481393+Aeroluna@users.noreply.github.com>
2025-01-05 04:07:48 -0700

    dont clear track frameupdate until song start fixes #135 fixes bug where events before start time are evaluated after update but before lateupdate due to callbacks being registered in StandardLevelGameplayManager, causing tracks to be updated and then immediately unupdated before anything can use updated value

--- Heck/Animation/TrackUpdateManager.cs
+++ Heck/Animation/TrackUpdateManager.cs
@@ -6,19 +6,35 @@ namespace Heck.Animation;
 
 internal class TrackUpdateManager : ILateTickable
 {
+    private readonly AudioTimeSyncController _audioTimeSyncController;
     private readonly HashSet<Track> _tracks;
 
+    private bool _songStarted;
+
     [UsedImplicitly]
-    private TrackUpdateManager(Dictionary<string, Track> beatmapTracks)
+    private TrackUpdateManager(Dictionary<string, Track> beatmapTracks, AudioTimeSyncController audioTimeSyncController)
     {
         _tracks = [..beatmapTracks.Values];
+        _audioTimeSyncController = audioTimeSyncController;
+        audioTimeSyncController.stateChangedEvent += OnSongStarted;
     }
 
     public void LateTick()
     {
+        if (!_songStarted)
+        {
+            return;
+        }
+
         foreach (Track track in _tracks)
         {
             track.UpdatedThisFrame = false;
         }
+    }
+
+    private void OnSongStarted()
+    {
+        _audioTimeSyncController.stateChangedEvent -= OnSongStarted;
+        _songStarted = true;
     }
 }
