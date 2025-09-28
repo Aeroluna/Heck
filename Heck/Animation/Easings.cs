8b40388dcaf257669b7ae867e550553680ca1d48
OwenTheProgrammer <owenthegamedev@gmail.com>
2025-09-28 18:10:07 -0400

    Updated easing functions to work/perform better

--- Heck/Animation/Easings.cs
+++ Heck/Animation/Easings.cs
@@ -58,24 +58,27 @@ public static class Easings
     private const float PI = Math.PI;
 
     /// <summary>
-    ///     Modeled after the overshooting cubic y = x^3-x*sin(x*pi)
+    ///     Dips down to (x:0.53, y:-0.379), shooting quickly up to 1. Like jumping up from a trampoline
     /// </summary>
     public static float EaseInBack(float p)
     {
         return (p * p * p) - (p * Math.Sin(p * PI));
     }
 
+    /// <summary>
+    ///     Bouncing from 0 up to 1. Like the Solitaire bouncing cards effect in reverse.
+    /// </summary>
     public static float EaseInBounce(float p)
     {
         return 1 - EaseOutBounce(1 - p);
     }
 
     /// <summary>
-    ///     Modeled after shifted quadrant IV of unit circle
+    ///     Bottom half of a unit circle centered around (x:0, y:1). Like riding the shape of a perfect bowl
     /// </summary>
     public static float EaseInCirc(float p)
     {
-        return 1 - Math.Sqrt(1 - (p * p));
+        return 1f - Math.Sqrt(Math.Max(Math.Epsilon, 1f - p * p));
     }
 
     /// <summary>
@@ -95,17 +98,16 @@ public static class Easings
     }
 
     /// <summary>
-    ///     Modeled after the expo function y = 2^(10(x - 1))
+    ///     Very slow climb from 0 to 1, exponentially accelerating at the end. Like magnets flying together
     /// </summary>
     public static float EaseInExpo(float p)
     {
-        return p <= 0.0f ? p : Math.Pow(2, 10 * (p - 1));
+        const float S = 1f / 1023f;
+        return p <= 0.0f ? p : (Math.Pow(2f, 10f * p) * S - S);
     }
 
     /// <summary>
-    ///     Modeled after the piecewise overshooting cubic function:
-    ///     y = (1/2)*((2x)^3-(2x)*sin(2*x*pi))           ; [0, 0.5)
-    ///     y = (1/2)*(1-((1-x)^3-(1-x)*sin((1-x)*pi))+1) ; [0.5, 1]
+    ///     Drops down to (x:0.265 y:-0.190), overshoots to (x:0.735 y:1.190), smoothly landing to 1. Like throwing someone upward aiming for 1
     /// </summary>
     public static float EaseInOutBack(float p)
     {
@@ -121,6 +123,9 @@ public static class Easings
         }
     }
 
+    /// <summary>
+    ///     Bouncing from the 0 line to the 1 line. Like if you bounced from a trampoline on a planet to another trampoline on another planet in mario galaxy
+    /// </summary>
     public static float EaseInOutBounce(float p)
     {
         if (p < 0.5f)
@@ -132,18 +137,17 @@ public static class Easings
     }
 
     /// <summary>
-    ///     Modeled after the piecewise circ function
-    ///     y = (1/2)(1 - Math.Sqrt(1 - 4x^2))           ; [0, 0.5)
-    ///     y = (1/2)(Math.Sqrt(-(2x - 3)*(2x - 1)) + 1) ; [0.5, 1]
+    ///     Place a circle at (x:0.5 y:0.5) with radius 0.5, and shift the top half 1 unit to the right. Thats this function.
     /// </summary>
     public static float EaseInOutCirc(float p)
     {
-        if (p < 0.5f)
+        if(p < 0.5f)
         {
-            return 0.5f * (1 - Math.Sqrt(1 - (4 * (p * p))));
+            return 0.5f - Math.Sqrt(Math.Max(Math.Epsilon, 0.25f - p*p));
         }
 
-        return 0.5f * (Math.Sqrt(-((2 * p) - 3) * ((2 * p) - 1)) + 1);
+        p -= 1f;
+        return 0.5f + Math.Sqrt(Math.Max(Math.Epsilon, 0.25f - p*p));
     }
 
     /// <summary>
@@ -153,13 +157,9 @@ public static class Easings
     /// </summary>
     public static float EaseInOutCubic(float p)
     {
-        if (p < 0.5f)
-        {
-            return 4 * p * p * p;
-        }
-
-        float f = (2 * p) - 2;
-        return (0.5f * f * f * f) + 1;
+        float f = p - 0.5f;
+        float x = Math.Abs(f);
+        return ((4f * x - 6f) * x + 3f) * f + 0.5f;
     }
 
     /// <summary>
@@ -184,48 +184,46 @@ public static class Easings
     /// </summary>
     public static float EaseInOutExpo(float p)
     {
-        if (p == 0.0 || p >= 1.0)
+        //p = Math.Clamp01(p);
+        if(p > 1f)
         {
             return p;
         }
 
-        if (p < 0.5f)
+        p = p * 20f - 10f;
+        const float S = 512f / 1023f;
+
+        // Left half
+        if(p < 0f)
         {
-            return 0.5f * Math.Pow(2, (20 * p) - 10);
+            return 0.5f - (S - S * Math.Pow(2f, p));
         }
 
-        return (-0.5f * Math.Pow(2, (-20 * p) + 10)) + 1;
+        // Right half
+        return 0.5f + (S - S * Math.Pow(2f, -p));
     }
 
     /// <summary>
-    ///     Modeled after the piecewise quad
-    ///     y = (1/2)((2x)^2)             ; [0, 0.5)
-    ///     y = -(1/2)((2x-1)*(2x-3) - 1) ; [0.5, 1]
+    ///     A slightly faster smoothstep
     /// </summary>
     public static float EaseInOutQuad(float p)
     {
-        if (p < 0.5f)
-        {
-            return 2 * p * p;
-        }
-
-        return ((-2 * p * p) + (4 * p)) - 1;
+        float x = p - 0.5f;
+        return (x - x * Math.Abs(x)) * 2f + 0.5f;
     }
 
     /// <summary>
-    ///     Modeled after the piecewise quart
-    ///     y = (1/2)((2x)^4)        ; [0, 0.5)
-    ///     y = -(1/2)((2x-2)^4 - 2) ; [0.5, 1]
+    ///     Like a half pipe from 0 - 0.5, leading to an upside down half pipe from 0.5 - 1
     /// </summary>
     public static float EaseInOutQuart(float p)
     {
-        if (p < 0.5f)
-        {
-            return 8 * p * p * p * p;
-        }
-
-        float f = p - 1;
-        return (-8 * f * f * f * f) + 1;
+        float t = 0f;
+        float f = p - 0.5f;
+        float x = Math.Abs(f);
+        t = x * -8f + 16f;
+        t = t * x - 12f;
+        t = t * x + 4f;
+        return t * f + 0.5f;
     }
 
     /// <summary>
@@ -235,13 +233,14 @@ public static class Easings
     /// </summary>
     public static float EaseInOutQuint(float p)
     {
-        if (p < 0.5f)
-        {
-            return 16 * p * p * p * p * p;
-        }
-
-        float f = (2 * p) - 2;
-        return (0.5f * f * f * f * f * f) + 1;
+        float t = 0f;
+        float f = p - 0.5f;
+        float x = Math.Abs(f);
+        t = x * 16f - 40f;
+        t = t * x + 40f;
+        t = t * x - 20f;
+        t = t * x + 5f;
+        return t * f + 0.5f;
     }
 
     /// <summary>
@@ -249,7 +248,8 @@ public static class Easings
     /// </summary>
     public static float EaseInOutSine(float p)
     {
-        return 0.5f * (1 - Math.Cos(p * PI));
+        float f = Math.Sin(HALFPI * p);
+        return f * f;
     }
 
     /// <summary>
@@ -277,15 +277,15 @@ public static class Easings
     }
 
     /// <summary>
-    ///     Modeled after quarter-cycle of sine wave
+    ///     An upside-down cosine wave starting at 0, hitting 1, and peaking at 2
     /// </summary>
     public static float EaseInSine(float p)
     {
-        return Math.Sin((p - 1) * HALFPI) + 1;
+        return 1f - Math.Cos(HALFPI * p);
     }
 
     /// <summary>
-    ///     Modeled after the line y = x
+    ///     Modeled after a straight line y = x
     /// </summary>
     public static float EaseLinear(float p)
     {
@@ -301,32 +301,49 @@ public static class Easings
         return 1 - ((f * f * f) - (f * Math.Sin(f * PI)));
     }
 
+    /// <summary>
+    ///     Bouncing from y=0 up to y=1. Like the Solitaire card bouncing when you win
+    ///     Collision points: 4/11, 8/11, 9/11, 1.0
+    /// </summary>
     public static float EaseOutBounce(float p)
     {
-        return p switch
-        {
-            < 4 / 11.0f => (121 * p * p) / 16.0f,
-            < 8 / 11.0f => (((363 / 40.0f) * p * p) - ((99 / 10.0f) * p)) + (17 / 5.0f),
-            < 9 / 10.0f => (((4356 / 361.0f) * p * p) - ((35442 / 1805.0f) * p)) + (16061 / 1805.0f),
-            _ => (((54 / 5.0f) * p * p) - ((513 / 25.0f) * p)) + (268 / 25.0f)
-        };
+        // First wave
+        float a = (121f / 16f) * p * p;
+        float x = a;
+
+        // Second wave
+        float q1 = p - (6f/11f);
+        float b = (363f / 40f) * q1 * q1 + (7f / 10f);
+        x = (b < x) ? b : x;
+
+        // Third wave
+        float q2 = p - (179f/220f);
+        float c = (4356f / 361f) * q2 * q2 + (91f / 100f);
+        x = (c < x) ? c : x;
+
+        // Fourth wave
+        float q3 = p - (19f/20f);
+        float d = (54f / 5f) * q3 * q3 + (973f / 1000f);
+        x = (d < x) ? d : x;
+
+        return x;
     }
 
     /// <summary>
-    ///     Modeled after shifted quadrant II of unit circle
+    ///     Modeled after the top half of a unit circle centered at (x:1, y:0)
     /// </summary>
     public static float EaseOutCirc(float p)
     {
-        return Math.Sqrt((2 - p) * p);
+        return Math.Sqrt(Math.Max(Math.Epsilon, (2f - p) * p));
     }
 
     /// <summary>
-    ///     Modeled after the cubic y = (x - 1)^3 + 1
+    ///     Modeled after the cubic y = 1 - (1 - x)^3
     /// </summary>
     public static float EaseOutCubic(float p)
     {
-        float f = p - 1;
-        return (f * f * f) + 1;
+        float f = 1f - p;
+        return 1f - (f * f * f);
     }
 
     /// <summary>
@@ -338,41 +355,42 @@ public static class Easings
     }
 
     /// <summary>
-    ///     Modeled after the expo function y = -2^(-10x) + 1
+    ///     Very sharp climb from 0 to 1, with an exponential slow down. Like a rocket landing on y=1
     /// </summary>
     public static float EaseOutExpo(float p)
     {
-        return p >= 1.0f ? p : 1 - Math.Pow(2, -10 * p);
+        const float S = 1024f / 1023f;
+        return p > 1.0f ? p : (S - S * Math.Pow(2f, -10f * p));
     }
 
     /// <summary>
-    ///     Modeled after the parabola y = -x^2 + 2x
+    ///     Modeled after the parabola y = 2x - x^2
     /// </summary>
     public static float EaseOutQuad(float p)
     {
-        return -(p * (p - 2));
+        return (2f - p) * p;
     }
 
     /// <summary>
-    ///     Modeled after the quart y = 1 - (x - 1)^4
+    ///     Modeled after the quart y = 1 - (1 - x)^4
     /// </summary>
     public static float EaseOutQuart(float p)
     {
-        float f = p - 1;
-        return (f * f * f * (1 - p)) + 1;
+        float f = 1f - p;
+        return 1f - (f * f * f * f);
     }
 
     /// <summary>
-    ///     Modeled after the quint y = (x - 1)^5 + 1
+    ///     Modeled after the quint y = 1 - (1 - x)^5
     /// </summary>
     public static float EaseOutQuint(float p)
     {
-        float f = p - 1;
-        return (f * f * f * f * f) + 1;
+        float f = 1f - p;
+        return 1f - (f * f * f * f * f);
     }
 
     /// <summary>
-    ///     Modeled after quarter-cycle of sine wave (different phase)
+    ///     Modeled after a sine wave, starting at 0, peaking at 1
     /// </summary>
     public static float EaseOutSine(float p)
     {
@@ -380,7 +398,7 @@ public static class Easings
     }
 
     /// <summary>
-    ///     It's either 1, or it's not
+    ///     Always rounds down to a whole number
     /// </summary>
     public static float EaseStep(float p)
     {
